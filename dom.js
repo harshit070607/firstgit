0c87f5cc0b18d5c6f7797f726d9c06b44e9d3622
harshit070607 <harshitsoni0706@gamil.com>
2023-05-10 00:50:50 +0530

    commit4

--- dom.js
+++ dom.js
@@ -1,12 +1,12 @@
 
 var a = document.getElementById('main-header')
-console.log(a);
+// console.log(a);
 
 var b = document.getElementById('main')
-console.log(b);
+// console.log(b);
 
 var c = document.getElementsByClassName('list-group-item')
-console.log(c);
+// console.log(c);
 
 c[2].style.backgroundColor = 'green';
 for(var i = 0;i<c.length;i++)
@@ -16,11 +16,18 @@ for(var i = 0;i<c.length;i++)
 
 var d = document.getElementsByClassName('list-group-item2')
 
-console.log(d);
+// console.log(d);
 d[0].style.backgroundColor='red';
 
 var e = document.getElementsByTagName('li');
-console.log(e);
+// console.log(e);
 e[4].style.fontWeight='900';
+
+var f = document.querySelectorAll('li:nth-child(2)');
+
+f[0].style.backgroundColor='green';
+
+var g = document.querySelectorAll('li:nth-child(3)');
+g[0].style.display='none';
 
 
