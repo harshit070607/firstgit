72ed3ef215b6e903136787e7c9feef061e3822d4
harshit070607 <harshitsoni0706@gamil.com>
2023-05-09 18:53:51 +0530

    commit2

--- dom.js
+++ dom.js
@@ -2,5 +2,14 @@
 var a = document.getElementById('main-header')
 console.log(a);
 
-var b = document.getElementById('items')
+var b = document.getElementById('main')
 console.log(b);
+
+var c = document.getElementsByClassName('list-group-item')
+console.log(c);
+
+c[2].style.backgroundColor = 'green';
+for(var i = 0;i<c.length;i++)
+{
+    c[i].style.fontWeight = '700';
+}
