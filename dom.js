7473bdf96c64fe08e24760dfa6b9de35986f6668
harshit070607 <harshitsoni0706@gamil.com>
2023-05-12 00:46:47 +0530

    commit6

--- dom.js
+++ dom.js
@@ -1,40 +1,70 @@
 
-var a = document.getElementById('main-header')
-// console.log(a);
+// var a = document.getElementById('main-header')
+// // console.log(a);
 
-var b = document.getElementById('main')
-// console.log(b);
+// var b = document.getElementById('main')
+// // console.log(b);
 
-var c = document.getElementsByClassName('list-group-item')
-// console.log(c);
+// var c = document.getElementsByClassName('list-group-item')
+// // console.log(c);
 
-c[2].style.backgroundColor = 'green';
-for(var i = 0;i<c.length;i++)
-{
-    c[i].style.fontWeight = '700';
-}
+// c[2].style.backgroundColor = 'green';
+// for(var i = 0;i<c.length;i++)
+// {
+//     c[i].style.fontWeight = '700';
+// }
 
-var d = document.getElementsByClassName('list-group-item2')
+// var d = document.getElementsByClassName('list-group-item2')
 
-// console.log(d);
-d[0].style.backgroundColor='red';
+// // console.log(d);
+// d[0].style.backgroundColor='red';
 
-var e = document.getElementsByTagName('li');
-// console.log(e);
-e[4].style.fontWeight='900';
+// var e = document.getElementsByTagName('li');
+// // console.log(e);
+// e[4].style.fontWeight='900';
 
-var f = document.querySelectorAll('li:nth-child(2)');
+// var f = document.querySelectorAll('li:nth-child(2)');
 
-// f[0].style.backgroundColor='green';
-f[0].style.color = "green";
+// // f[0].style.backgroundColor='green';
+// f[0].style.color = "green";
 
-var g = document.querySelectorAll('li:nth-child(3)');
-// g[0].style.display='none';
+// var g = document.querySelectorAll('li:nth-child(3)');
+// // g[0].style.display='none';
 
-var odd = document.querySelectorAll('li:nth-child(odd)');
-for(var i=0; i<odd.length ; i++)
-{
-    odd[i].style.backgroundColor="green";
-}
+// var odd = document.querySelectorAll('li:nth-child(odd)');
+// for(var i=0; i<odd.length ; i++)
+// {
+//     odd[i].style.backgroundColor="green";
+// }
+
+var h = document.querySelector('#main');
+console.log(h.parentElement);
+console.log(h.lastElementChild);
+console.log(h.lastChild);
+console.log(h.firstElementChild);
+console.log(h.firstChild);
+console.log(h.nextElementSibling);
+console.log(h.nextSibling);
+console.log(h.previousElementSibling);
+console.log(h.previousSibling);
+
+var i = document.createElement('div');
+i.className='hello1';
+i.id = 'hello2';
+i.setAttribute('title','createElement');
+console.log(i);
+
+var j = document.createTextNode('Hello world');
+
+i.appendChild(j);
+var k = document.querySelector('header .container');
+var l = document.querySelector('#header-title');
+
+k.insertBefore(i,l);
+
+var m = document.querySelector('#items');
+var n = document.querySelector('.list-group-item');
+
+m.insertBefore(i,n);
 
 
