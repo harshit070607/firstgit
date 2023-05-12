3b9ffc687262ec426c62221ec34485b1bcff7610
harshit070607 <harshitsoni0706@gamil.com>
2023-05-13 00:52:07 +0530

    commit7

--- dom.js
+++ dom.js
@@ -37,34 +37,80 @@
 //     odd[i].style.backgroundColor="green";
 // }
 
-var h = document.querySelector('#main');
-console.log(h.parentElement);
-console.log(h.lastElementChild);
-console.log(h.lastChild);
-console.log(h.firstElementChild);
-console.log(h.firstChild);
-console.log(h.nextElementSibling);
-console.log(h.nextSibling);
-console.log(h.previousElementSibling);
-console.log(h.previousSibling);
+// var h = document.querySelector('#main');
+// console.log(h.parentElement);
+// console.log(h.lastElementChild);
+// console.log(h.lastChild);
+// console.log(h.firstElementChild);
+// console.log(h.firstChild);
+// console.log(h.nextElementSibling);
+// console.log(h.nextSibling);
+// console.log(h.previousElementSibling);
+// console.log(h.previousSibling);
 
-var i = document.createElement('div');
-i.className='hello1';
-i.id = 'hello2';
-i.setAttribute('title','createElement');
-console.log(i);
+// var i = document.createElement('div');
+// i.className='hello1';
+// i.id = 'hello2';
+// i.setAttribute('title','createElement');
+// console.log(i);
 
-var j = document.createTextNode('Hello world');
+// var j = document.createTextNode('Hello world');
 
-i.appendChild(j);
-var k = document.querySelector('header .container');
-var l = document.querySelector('#header-title');
+// i.appendChild(j);
+// var k = document.querySelector('header .container');
+// var l = document.querySelector('#header-title');
 
-k.insertBefore(i,l);
+// k.insertBefore(i,l);
 
-var m = document.querySelector('#items');
-var n = document.querySelector('.list-group-item');
+// var m = document.querySelector('#items');
+// var n = document.querySelector('.list-group-item');
 
-m.insertBefore(i,n);
+// m.insertBefore(i,n);
 
+
+var form = document.getElementById('addForm');
+var itemlist = document.getElementById('items');
+
+form.addEventListener('submit',addItem);
+itemlist.addEventListener('click',removeItem);
+
+function addItem(e){
+    e.preventDefault();
+
+var newItem = document.getElementById('item').value;
+
+var li = document.createElement('li');
+
+li.className='list-group-item';
+
+li.appendChild(document.createTextNode(newItem));
+
+itemlist.appendChild(li);
+
+var deletebtn = document.createElement('button');
+
+deletebtn.className='btn btn-danger btn-sm float-right delete';
+
+var textAtbtn = document.createTextNode('X');
+
+deletebtn.appendChild(textAtbtn);
+
+li.appendChild(deletebtn);
+
+}
+
+
+
+function removeItem(e){
+    if(e.target.classList.contains('delete'))
+    {
+        if(confirm('Are you sure?'))
+        {
+            var rem = e.target.parentElement;
+            itemlist.removeChild(rem);
+        }
+    }
+
+}
+ 
 
